2316dd217af3e6013b4280d312272e434068214a
Pierrrrrrrot <pierre.reliquet@gmail.com>
2013-03-20 21:34:16 +0100

    First activity (menu) and basic configuration files

---
+++ src/org/pierrrrrrrot/loanmanager/utils/Utils.java
@@ -0,0 +1,9 @@
+package org.pierrrrrrrot.loanmanager.utils;
+
+public class Utils {
+
+    public static boolean isNullOrEmpty(String s) {
+        return ((s == null) || (s.trim().length() == 0));
+    }
+
+}
