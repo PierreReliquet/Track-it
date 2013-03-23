5cdca1965ffc87216c2baa78ca7bd7a14aad3bdf
Pierrrrrrrot <pierre.reliquet@gmail.com>
2013-03-23 19:08:20 +0100

    Implementation of the about

--- src/org/pierrrrrrrot/loanmanager/Menu.java
+++ src/org/pierrrrrrrot/loanmanager/Menu.java
@@ -8,13 +8,21 @@ import org.pierrrrrrrot.loanmanager.activities.ListProducts;
 import org.pierrrrrrrot.loanmanager.dao.DBHelper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Menu extends Activity {
+
+    private static final String LOANS_MANAGER_MAIL_TITLE = "Loans Manager ";
+    private static final String[] AUTHOR_EMAIL = { "pierre.reliquet@gmail.com" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,11 +102,63 @@ public class Menu extends Activity {
         startActivity(intent);
     }
 
-    // @Override
-    // public boolean onCreateOptionsMenu(Menu menu) {
-    // // Inflate the menu; this adds items to the action bar if it is present.
-    // getMenuInflater().inflate(R.menu.menu, menu);
-    // return true;
-    // }
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.about, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_about:
+            try {
+                showAbout();
+                return true;
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showAbout() throws NameNotFoundException {
+        PackageInfo pInfo = getPackageManager().getPackageInfo(
+                getPackageName(), 0);
+        final String version = pInfo.versionName;
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.menu_about_title)
+                .setMessage(
+                        String.format(Menu.this
+                                .getString(R.string.menu_about_message),
+                                version))
+                .setNegativeButton(R.string.close,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                // Nothing to do just close the dialog
+                            }
+                        })
+                .setPositiveButton(R.string.contact,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                Intent sendIntent = new Intent(
+                                        Intent.ACTION_SEND);
+                                sendIntent.setType("text/plain");
+                                sendIntent.putExtra(Intent.EXTRA_EMAIL,
+                                        AUTHOR_EMAIL);
+                                sendIntent.putExtra(Intent.EXTRA_SUBJECT,
+                                        LOANS_MANAGER_MAIL_TITLE + version);
+                                startActivity(Intent.createChooser(sendIntent,
+                                        "Email:"));
+                            }
+                        }).show();
+    }
 }
