ad1e61f21ce7637c0224edcca013d91b82073cda
Pierrrrrrrot <pierre.reliquet@gmail.com>
2013-03-23 15:02:55 +0100

    Removing menu on lists

--- src/org/pierrrrrrrot/loanmanager/activities/ListProducts.java
+++ src/org/pierrrrrrrot/loanmanager/activities/ListProducts.java
@@ -11,9 +11,6 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 
 public class ListProducts extends Activity {
@@ -45,29 +42,4 @@ public class ListProducts extends Activity {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.list_products, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case android.R.id.home:
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
