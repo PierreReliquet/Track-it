a224ad1cfa5d347c707f177e9edd407995fd0117
Pierre Reliquet <pierre.reliquet@gmail.com>
2013-06-22 10:46:16 +0200

    Disabling buttons on main menu when there is no content in it.

--- src/fr/free/pierre/reliquet/trackit/Menu.java
+++ src/fr/free/pierre/reliquet/trackit/Menu.java
@@ -33,150 +33,176 @@ import fr.free.pierre.reliquet.trackit.activities.CloseLoan;
 import fr.free.pierre.reliquet.trackit.activities.ListBorrowers;
 import fr.free.pierre.reliquet.trackit.activities.ListLoans;
 import fr.free.pierre.reliquet.trackit.activities.ListProducts;
+import fr.free.pierre.reliquet.trackit.dao.BorrowersDAO;
 import fr.free.pierre.reliquet.trackit.dao.DBHelper;
+import fr.free.pierre.reliquet.trackit.dao.LoansDAO;
+import fr.free.pierre.reliquet.trackit.dao.ProductsDAO;
+import fr.free.pierre.reliquet.trackit.enums.CloseLoanFilter;
 
 public class Menu extends Activity {
-    
-    private static final String[] AUTHOR_EMAIL             = { "pierre.reliquet@gmail.com" };
-    private static final String   LOANS_MANAGER_MAIL_TITLE = "Track-it ";
-    
-    private void init() {
-        // Create dbs if does not exists
-        DBHelper.globalInit(this);
-    }
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.init();
-        this.setContentView(R.layout.activity_menu);
-        // Initialization of the DBHelper
-        Button addLoan = (Button) this.findViewById(R.id.menu_add_loan);
-        addLoan.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Menu.this.startAddLoan();
-            }
-        });
-        
-        Button closeLoan = (Button) this.findViewById(R.id.menu_close_loan);
-        closeLoan.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Menu.this.startCloseLoan();
-            }
-        });
-        
-        Button listBorrowers = (Button) this
-                .findViewById(R.id.menu_list_borrowers);
-        listBorrowers.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Menu.this.startListBorrowers();
-            }
-        });
-        
-        Button listProducts = (Button) this
-                .findViewById(R.id.menu_list_products);
-        listProducts.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Menu.this.startListProducts();
-            }
-        });
-        
-        Button listLoans = (Button) this.findViewById(R.id.menu_list_loans);
-        listLoans.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Menu.this.startListLoans();
-            }
-        });
-        
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(android.view.Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        this.getMenuInflater().inflate(R.menu.about, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_about:
-                try {
-                    this.showAbout();
-                    return true;
-                } catch (NameNotFoundException e) {
-                    e.printStackTrace();
-                    return false;
-                }
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    
-    private void showAbout() throws NameNotFoundException {
-        PackageInfo pInfo = this.getPackageManager().getPackageInfo(
-                this.getPackageName(), 0);
-        final String version = pInfo.versionName;
-        final SpannableString message = new SpannableString(String.format(
-                Menu.this.getString(R.string.menu_about_message), version));
-        Linkify.addLinks(message, Linkify.WEB_URLS);
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.menu_about_title)
-                .setMessage(message)
-                .setNegativeButton(R.string.close,
-                        new DialogInterface.OnClickListener() {
-                            
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                    int which) {
-                                // Nothing to do just close the dialog
-                            }
-                        })
-                .setPositiveButton(R.string.contact,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                    int which) {
-                                Intent sendIntent = new Intent(
-                                        Intent.ACTION_SEND);
-                                sendIntent.setType("text/plain");
-                                sendIntent.putExtra(Intent.EXTRA_EMAIL,
-                                        AUTHOR_EMAIL);
-                                sendIntent.putExtra(Intent.EXTRA_SUBJECT,
-                                        LOANS_MANAGER_MAIL_TITLE + version);
-                                Menu.this.startActivity(Intent.createChooser(
-                                        sendIntent, "Email:"));
-                            }
-                        }).show();
-    }
-    
-    private void startAddLoan() {
-        Intent intent = new Intent(this, AddLoan.class);
-        this.startActivity(intent);
-    }
-    
-    private void startCloseLoan() {
-        Intent intent = new Intent(this, CloseLoan.class);
-        this.startActivity(intent);
-    }
-    
-    private void startListBorrowers() {
-        Intent intent = new Intent(this, ListBorrowers.class);
-        this.startActivity(intent);
-    }
-    
-    private void startListLoans() {
-        Intent intent = new Intent(this, ListLoans.class);
-        this.startActivity(intent);
-    }
-    
-    private void startListProducts() {
-        Intent intent = new Intent(this, ListProducts.class);
-        this.startActivity(intent);
-    }
+
+	private static final String[] AUTHOR_EMAIL = { "pierre.reliquet@gmail.com" };
+	private static final String LOANS_MANAGER_MAIL_TITLE = "Track-it ";
+	
+	private Button closeLoan;
+	private Button listBorrowers;
+	private Button listLoans;
+	private Button listProducts;
+
+	private void init() {
+		// Create dbs if does not exists
+		DBHelper.globalInit(this);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.init();
+		this.setContentView(R.layout.activity_menu);
+		// Initialization of the DBHelper
+		Button addLoan = (Button) this.findViewById(R.id.menu_add_loan);
+		addLoan.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Menu.this.startAddLoan();
+			}
+		});
+
+		closeLoan = (Button) this.findViewById(R.id.menu_close_loan);
+		closeLoan.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Menu.this.startCloseLoan();
+			}
+		});
+
+		listBorrowers = (Button) this
+				.findViewById(R.id.menu_list_borrowers);
+		listBorrowers.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Menu.this.startListBorrowers();
+			}
+		});
+
+		listProducts = (Button) this
+				.findViewById(R.id.menu_list_products);
+		listProducts.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Menu.this.startListProducts();
+			}
+		});
+
+		listLoans = (Button) this.findViewById(R.id.menu_list_loans);
+		listLoans.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Menu.this.startListLoans();
+			}
+		});
+		
+		updateButtons();
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updateButtons();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		this.getMenuInflater().inflate(R.menu.about, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_about:
+			try {
+				this.showAbout();
+				return true;
+			} catch (NameNotFoundException e) {
+				e.printStackTrace();
+				return false;
+			}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void showAbout() throws NameNotFoundException {
+		PackageInfo pInfo = this.getPackageManager().getPackageInfo(
+				this.getPackageName(), 0);
+		final String version = pInfo.versionName;
+		final SpannableString message = new SpannableString(String.format(
+				Menu.this.getString(R.string.menu_about_message), version));
+		Linkify.addLinks(message, Linkify.WEB_URLS);
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.menu_about_title)
+				.setMessage(message)
+				.setNegativeButton(R.string.close,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// Nothing to do just close the dialog
+							}
+						})
+				.setPositiveButton(R.string.contact,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								Intent sendIntent = new Intent(
+										Intent.ACTION_SEND);
+								sendIntent.setType("text/plain");
+								sendIntent.putExtra(Intent.EXTRA_EMAIL,
+										AUTHOR_EMAIL);
+								sendIntent.putExtra(Intent.EXTRA_SUBJECT,
+										LOANS_MANAGER_MAIL_TITLE + version);
+								Menu.this.startActivity(Intent.createChooser(
+										sendIntent, "Email:"));
+							}
+						}).show();
+	}
+
+	private void startAddLoan() {
+		Intent intent = new Intent(this, AddLoan.class);
+		this.startActivity(intent);
+	}
+
+	private void startCloseLoan() {
+		Intent intent = new Intent(this, CloseLoan.class);
+		intent.putExtra(CloseLoan.CLOSE_LOAN_FILTER_EXTRA,
+				CloseLoanFilter.ALL.name());
+		this.startActivity(intent);
+	}
+
+	private void startListBorrowers() {
+		Intent intent = new Intent(this, ListBorrowers.class);
+		this.startActivity(intent);
+	}
+
+	private void startListLoans() {
+		Intent intent = new Intent(this, ListLoans.class);
+		this.startActivity(intent);
+	}
+
+	private void startListProducts() {
+		Intent intent = new Intent(this, ListProducts.class);
+		this.startActivity(intent);
+	}
+	
+	private void updateButtons() {
+		closeLoan.setEnabled(LoansDAO.getInstance().getNumberOfNonTerminatedLoans() != 0);
+		listBorrowers.setEnabled(BorrowersDAO.getInstance().getNumberOfBorrowers() != 0);
+		listLoans.setEnabled(LoansDAO.getInstance().getNumberOfLoans() != 0);
+		listProducts.setEnabled(ProductsDAO.getInstance().getNumberOfProducts() != 0);
+	}
 }
